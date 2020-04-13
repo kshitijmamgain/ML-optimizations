a0f10f093a50073063ed3f45566688b2112f29c9
Kshitij Mamgain <kshitij.dsa@gmail.com>
2020-04-13 18:21:27 -0400

    lgbm class

---
+++ lgbmclass.py
@@ -0,0 +1,403 @@
+
+# coding: utf-8
+''' This class tunes hyperparamter for LightGBM ML algorithm for Higgs dataset'''
+import csv
+from timeit import default_timer as timer
+import random
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import auc, accuracy_score, roc_auc_score, roc_curve, confusion_matrix, precision_recall_curve
+from hyperopt import STATUS_OK, STATUS_FAIL, hp, tpe, Trials, fmin
+import optuna.integration.lightgbm as lgbo
+import optuna
+import matplotlib.pyplot as plt
+
+
+# defining constant
+MAX_EVALS = 5
+N_FOLDS = 3
+NUM_BOOST_ROUNDS = 10000
+EARLY_STOPPING_ROUNDS = 100
+SEED = 47
+RESULT_PATH = '/content/lgbm.csv'
+FILE_PATH = "drive/My Drive/Colab Notebooks/train_test_files_sample.csv" 
+OBJECTIVE_LOSS = 'binary' # use cross_entropy
+# "/home/jupyter/train_test_files/sample.csv" for google cloud
+
+# starting with storing the data as data frame
+df = pd.read_csv(FILE_PATH)
+df.drop(columns='Unnamed: 0', inplace=True)
+
+# making a smaller df for quick testing
+df_s, _ = train_test_split(df, random_state=30, train_size=0.01)
+train_X = df_s.drop(columns='0')
+train_y = df_s['0']
+
+# drop last columns
+def col_keep(df):
+    '''Removes the last 7 columns'''
+    return df.drop(columns=list(map(str, range(22, 29))), inplace=True) # removing 7 last columns
+
+
+# random search
+PARAM_GRID = {
+    'num_leaves': list(range(16, 196, 4)),
+    'max_bin': [254],
+    'lambda_l1': list(np.linspace(0, 1)),
+    'lambda_l2': list(np.linspace(0, 1)),
+    'min_data_in_leaf' : list(range(20, 500, 10)),
+    'class_weight': [None, 'balanced'],
+    'boosting_type': ['gbdt', 'goss'],
+    'learning_rate' : list(np.logspace(np.log(0.05), np.log(0.2), base=np.exp(1), num=1000)),
+    'feature_fraction': list(np.linspace(0.4, 1.0)),
+    'subsample_for_bin': list(range(20000, 300000, 20000)),
+    'bagging_freq': list(range(1, 7)),
+    'verbosity' : [0],
+    'objective' : [OBJECTIVE_LOSS]
+    }
+
+
+# Hyperopt Space
+H_SPACE = {
+    'num_leaves': hp.quniform('num_leaves', 16, 196, 4),
+    'max_bin' : hp.quniform('max_bin', 253, 254, 1), #if using CPU just set this to 254
+    'lambda_l1': hp.uniform('lambda_l1', 0.0, 1.0),
+    'lambda_l2': hp.uniform("lambda_l2", 0.0, 1.0),
+    'min_data_in_leaf' : hp.quniform('min_data_in_leaf', 20, 500, 10),
+    'class_weight': hp.choice('class_weight', [None, 'balanced']),
+    'boosting_type': hp.choice('boosting_type',
+                               [{'boosting_type': 'gbdt',
+                                 'subsample': hp.uniform('gdbt_subsample', 0.5, 1)},
+                                #{'boosting_type': 'dart',
+                                #'subsample': hp.uniform('dart_subsample', 0.5, 1)},
+                                {'boosting_type': 'goss', 'subsample': 1.0}]),
+    'learning_rate' : hp.loguniform('learning_rate', np.log(0.05), np.log(0.25)),
+    'subsample_for_bin': hp.quniform('subsample_for_bin', 20000, 300000, 20000),
+    'feature_fraction': hp.uniform('feature_fraction', 0.4, 1.0),
+    'bagging_freq': hp.uniform('bagging_freq', 1, 7),
+    'verbosity' : 0,
+    'objective' : OBJECTIVE_LOSS
+    }
+
+class Lgbmclass():
+    '''Parameter Tuning Class tunes the LightGBM model with different optimization techniques -
+    Hyperopt, Optuna and RandomSearch. At present method for CV using Hyperopt is defined'''
+    iteration = 0
+    def __init__(self, x_train, y_train):
+        '''Initializes the Parameter tuning class and also initializes LightGBM dataset object
+        Parameters
+        ----------
+        x_train: data (string, numpy array, pandas DataFrame, H2O DataTable's Frame, scipy.sparse
+        or list of numpy arrays) – Data source of Dataset. If string, it represents the path to
+        txt file.
+        y_train: label (list, numpy 1-D array, pandas Series / one-column DataFrame or None,
+        optional (default=None)) – Label of the data.'''
+
+        
+
+        # File to save first results
+        self.out_file = RESULT_PATH
+        of_connection = open(self.out_file, 'w')
+        writer = csv.writer(of_connection)
+
+        # Write the headers to the file
+        writer.writerow(['loss', 'params', 'iteration', 'estimators', 'train_time','optim_type'])
+        of_connection.close()
+
+        self.x_train = x_train
+        self.y_train = y_train
+        self.train_set = lgb.Dataset(data=train_X, label=train_y)
+
+    def lgb_crossval(self, params, optim_type):
+        '''lgb cross validation model
+        Paramters
+        ---------
+        params: Hyper parameters in dict type from different optimization methods
+        optim_type: Is the type of optimization called we use lgb integration for optuna type
+        Returns
+        ------
+        Loss, params, n_estimator, run_time'''
+        # initializing the timer
+         
+        start = timer()
+        if optim_type == 'optuna':
+            cv_results = lgbo.cv(params, self.train_set, num_boost_round=NUM_BOOST_ROUNDS,
+                                 nfold=N_FOLDS, early_stopping_rounds=EARLY_STOPPING_ROUNDS,
+                                 metrics=['auc', 'binary', 'xentropy'], seed=SEED)
+        else:
+            cv_results = lgb.cv(params, self.train_set, num_boost_round=NUM_BOOST_ROUNDS,
+                                nfold=N_FOLDS, early_stopping_rounds=EARLY_STOPPING_ROUNDS,
+                                metrics=['auc', 'binary', 'xentropy'], seed=SEED)
+        # store the runtime
+        run_time = timer() - start
+
+        # Extract the best score
+        best_score = np.max(cv_results['auc-mean'])
+
+        # Loss must be minimized
+        loss = 1 - best_score
+
+        # Boosting rounds that returned the highest cv score
+        n_estimators = int(np.argmax(cv_results['auc-mean']) + 1)
+        self.estimator = n_estimators
+
+        # Write to the csv file ('a' means append)
+        of_connection = open(self.out_file, 'a')
+        writer = csv.writer(of_connection)
+        writer.writerow([loss, params, self.iteration, n_estimators, run_time, optim_type])
+
+        return loss, params, n_estimators, run_time
+
+    def parameter_tuning(self, tune_type):
+        tuner = getattr(self, tune_type)
+        return(tuner())
+
+    def hyperopt_space(self):
+        '''A method to call the hyperopt optimization for the data
+        Parameters
+        ----------
+        fn_name: is the objective function to minimize defined with in the class function
+        space: is dictionary type hypeorpt space over which the search is done
+        algo: is the type of search algorithm
+        trials: Hyperopt base trials object
+        Returns
+        -------
+        result: best parameter that minimizes the fn_name over max_evals = MAX_EVALS FIXED FOR TESTING
+        trials: the database in which to store all the point evaluations of the search'''
+        fn_name, space, algo, trials='hyperopt_obj', H_SPACE, tpe.suggest, Trials()
+        fn = getattr(self, fn_name)
+        try:
+            result = fmin(fn=fn, space=space, algo=algo, max_evals=MAX_EVALS,
+                          trials=trials, rstate=np.random.RandomState(SEED))
+        except Exception as e:
+            return {'status': STATUS_FAIL, 'exception': str(e)}
+        self.params = trials.best_trial['result']['params']
+        self.params['n_estimator'] = self.estimator
+        return result, trials
+
+    def optuna_space(self):
+        '''Optuna search space'''
+        fn_name = 'optuna_obj'
+        fn = getattr(self, fn_name)
+        try:
+            study = optuna.create_study(direction='minimize')
+            study.optimize(fn, n_trials=MAX_EVALS)
+        except Exception as e:
+            return {'exception': str(e)}
+        self.params = study.best_params
+        self.params['n_estimator'] = self.estimator
+        return study
+
+    def random_space(self):
+        '''Random search space'''
+        # Dataframe to hold cv results
+        space = PARAM_GRID
+        random_results = pd.DataFrame(columns=['loss', 'params', 'iteration', 'estimators',
+                                               'time'], index=list(range(MAX_EVALS)))
+
+        # Iterate through the specified number of evaluations
+        for i in range(MAX_EVALS):
+
+            # Randomly sample parameters for gbm
+            params = {key: random.sample(value, 1)[0] for key, value in space.items()}
+            results_list = self.randomsrch_obj(params, i)
+
+            # Add results to next row in dataframe
+            random_results.loc[i, :] = results_list
+        #sort values by the loss
+        random_results.sort_values('loss', ascending = True, inplace = True)
+        self.params = random_results.loc[0, 'params']
+        self.params['n_estimator'] = self.estimator
+        return random_results
+
+    def hyperopt_obj(self, params):
+        """Objective function for Gradient Boosting Machine Hyperparameter Optimization"""
+
+        optim_type = 'Hyperopt'
+        self.iteration += 1
+
+        # Retrieve the subsample if present otherwise set to 1.0
+        subsample = params['boosting_type'].get('subsample', 1.0)
+
+        # Extract the boosting type
+        params['boosting_type'] = params['boosting_type']['boosting_type']
+        params['subsample'] = subsample
+
+        # Make sure parameters that need to be integers are integers
+        for parameter_name in ['num_leaves', 'subsample_for_bin', 'min_data_in_leaf',
+                               'max_bin', 'bagging_freq']:
+            params[parameter_name] = int(params[parameter_name])
+
+        # Perform n_folds cross validation
+        loss, params, n_estimators, run_time = self.lgb_crossval(params, optim_type)
+
+        # Dictionary with information for evaluation
+        return {'loss':loss, 'params':params, 'iteration':self.iteration,
+                'estimators':n_estimators, 'train_time':run_time, 'status':STATUS_OK}
+
+    def optuna_obj(self, trial):
+        '''Defining the parameters space inside the function for optuna optimization'''
+        params = {
+            'num_leaves': trial.suggest_int('num_leaves', 16, 196, 4),
+            'max_bin' : trial.suggest_int('max_bin', 63, 255, 64),
+            'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
+            'lambda_l2': trial.suggest_loguniform("lambda_l2", 1e-8, 10.0),
+            'min_data_in_leaf' : trial.suggest_int('min_data_in_leaf', 20, 500),
+            'class_weight': trial.suggest_categorical('class_weight', [None, 'balanced']),
+            'boosting_type': trial.suggest_categorical('boosting_type', ['gbdt', 'goss']),
+            # removed 'dart'
+            'learning_rate' : trial.suggest_loguniform('learning_rate', 0.05, 0.25),
+            'subsample_for_bin': trial.suggest_int('subsample_for_bin',20000, 300000, 20000),
+            'feature_fraction': trial.suggest_uniform("feature_fraction", 0.4, 1.0),
+            'bagging_freq': trial.suggest_int("bagging_freq", 1, 7),
+            'verbosity' : 0,
+            'objective' : OBJECTIVE_LOSS
+                }
+
+        optim_type = 'Optuna'
+        self.iteration += 1
+
+        # Make sure parameters that need to be integers are integers
+        for parameter_name in ['num_leaves', 'min_data_in_leaf',
+                               'max_bin', 'bagging_freq']:
+            params[parameter_name] = int(params[parameter_name])
+
+        # Perform n_folds cross validation
+        if params['boosting_type'] == 'goss':
+            params['subsample'] = 1
+        else:
+            params['subsample'] = trial.suggest_uniform('subsample', 0.5, 1)
+
+        loss, params, _, _ = self.lgb_crossval(params, optim_type)
+
+        return loss
+
+    def randomsrch_obj(self, params, iteration):
+        """Random search objective function. Takes in hyperparameters and returns a list
+        of results to be saved."""
+        optim_type = 'Random'
+        self.iteration += 1
+
+        # Subsampling (only applicable with 'goss')
+        subsample_dist = list(np.linspace(0.5, 1, 100))
+
+        if params['boosting_type'] == 'goss':
+            # Cannot subsample with goss
+            params['subsample'] = 1.0
+        else:
+            # Subsample supported for gdbt and dart
+            params['subsample'] = random.sample(subsample_dist, 1)[0]
+
+        # Perform n_folds cross validation
+        loss, params, n_estimators, run_time = self.lgb_crossval(params, optim_type)
+
+        # Return list of results
+        return [loss, params, iteration, n_estimators, run_time]
+
+    def train(self, x_test, y_test):
+        """This function evaluates the model on paramters and estimators
+        Parameters
+        ----------
+        x_test: test set; y_test: test label"""
+
+        for parameter_name in ['num_leaves', 'subsample_for_bin', 'min_data_in_leaf',
+                               'max_bin', 'bagging_freq']:
+            self.params[parameter_name] = int(self.params[parameter_name])
+        self.gbm = lgb.train(self.params, self.train_set,
+                             feature_name=['f' + str(i + 1) for i in range(train_X.shape[-1])])
+        self.pred = self.gbm.predict(x_test)
+        self.test_y = y_test
+        print("Model will be trained with best parameters obtained from Hyperopt ... \n\n\n")
+        print("Model trained with {} estimators on the following parameters: \n{}".format(self.estimator, self.params))
+
+    def evaluate(self):
+        """This function generates the evaluation report for the model"""
+        pred = self.pred
+        print('check pred')
+        (self.fpr, self.tpr, self.thresholds) = roc_curve(y_true=self.test_y, y_score=pred)
+        print('fpr, tpr, thresh check')
+        self.fnr = 1- self.tpr
+        print('fnr check')
+        self.roc_auc = auc(self.fpr, self.tpr)
+        print('roc_Auc check')
+        self.precision, self.recall, _ = precision_recall_curve(self.test_y, pred)
+        print('precision recall check')
+        self.pr_auc = auc(self.recall, self.precision)
+        print('pr_acu check')
+        eval_list = ['feature_importance','roc', 'prcurve', 'fpr_fnr']
+        for eval_name in eval_list:
+            func = getattr(self,eval_name)
+            func()
+        else:
+            print('Not valid evaluation type')
+
+    def feature_importance(self):
+        print('Plotting feature importances...')
+        ax = lgb.plot_importance(self.gbm, max_num_features=10)
+        plt.savefig('feature_importance.png')
+        
+
+    def roc(self):
+        fpr, tpr, roc = self.fpr, self.tpr, self.roc_auc
+
+        plt.figure(figsize=(16, 8))
+        lw = 2
+
+        plt.plot(fpr, tpr, color='darkorange',
+                 lw=lw, label='ROC curve (area = %0.2f)' % self.roc_auc, alpha=0.5)
+
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--', alpha=0.5)
+
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.grid(True)
+        plt.xlabel('False Positive Rate', fontsize=16)
+        plt.ylabel('True Positive Rate', fontsize=16)
+        plt.title('Receiver operating characteristic', fontsize=20)
+        plt.legend(loc="lower right", fontsize=16)
+        plt.savefig('roc.png')
+        
+    def prcurve(self):
+        recall, precision, pr_auc = self.recall, self.precision, self.pr_auc
+        # plot the precision-recall curves
+        no_skill = len(test_y[test_y==1]) / len(test_y)
+        plt.figure(figsize = (16,8))
+        plt.plot([0, 1], [no_skill, no_skill], color='navy', linestyle='--',
+                 alpha=0.5)
+        plt.plot(recall, precision, color='darkorange',
+                 label='ROC curve (area = %0.2f)'% pr_auc, alpha=0.5)
+        # axis labels
+        plt.title('Precision Recall Curve', size = 20)
+        plt.xlabel('Recall', fontsize=16)
+        plt.ylabel('Precision', fontsize=16)
+        plt.grid(True)
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        # show the legend
+        plt.legend(fontsize=16)
+        plt.savefig('prcurve.png')
+    def fpr_fnr(self):
+        lw = 2
+        fpr, fnr, thresholds = self.fpr, self.fnr, self.thresholds
+        plt.figure(figsize = (16,8))
+        plt.plot(thresholds, fpr, color='blue', lw=lw, label='FPR', alpha=0.5)
+        plt.plot(thresholds, fnr, color='green', lw=lw, label='FNR', alpha=0.5)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        plt.grid(True)
+        plt.xlabel('Threshold', fontsize=16)
+        plt.ylabel('Error Rate', fontsize=16)
+        plt.title('FPR-FNR curves', fontsize=20)
+        plt.legend(loc="lower left", fontsize=16)
+        plt.savefig('fpr-fnr.png')
+
+
+
+obj = Lgbmclass(train_X, train_y)
+
